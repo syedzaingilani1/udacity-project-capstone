07c0851bc86af0966b1b6be625ff88eb72548394
root <root@ip-172-31-73-3.ec2.internal>
2020-09-10 14:12:05 +0000

    updated

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,6 +3,9 @@
 # docker path
 dockerpath=talha110/talha-project-capstone
 
+# Step 2:
+# Authenticate & tag
+docker login --username talha110
 echo "Docker ID and Image: $dockerpath"
 
 docker tag talha110/project-capstone talha110/project-capstone:latest
