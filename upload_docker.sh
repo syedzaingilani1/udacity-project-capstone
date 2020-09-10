e72eeb394e4f62bf159eb129fdc4f947a4290f51
root <root@ip-172-31-73-3.ec2.internal>
2020-09-10 13:55:45 +0000

    docker upload file updated

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
-export dockerpath=talha110/talha-project-capstone 
+# docker path
+dockerpath=talha110/talha-project-capstone
 
-# Step 2:  
-# Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-docker login --username talha110
-docker tag talha-project-capstone  $dockerpath
 
-# Step 3:
-# Push image to a docker repository
-docker push $dockerpath
+docker tag talha110/talha-project-capstone talha110/talha-project-capstone:v1
+
+docker push talha110/talha-project-capstone
